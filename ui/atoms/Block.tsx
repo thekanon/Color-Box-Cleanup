61fe16d95aa81a3d0bdc9e45fc94b32de7531c53
thekanon <dlengjs12@naver.com>
2024-05-22 07:57:19 +0900

    스타일 수정

--- ui/atoms/Block.tsx
+++ ui/atoms/Block.tsx
@@ -2,6 +2,25 @@
 import React from "react";
 import { BlockProps } from "@/types/componentTypes";
 
+const colors = {
+  red: "bg-red", //
+  green: "bg-green",
+  blue: "bg-blue",
+  yellow: "bg-yellow",
+  purple: "bg-purple",
+  cyan: "bg-cyan",
+  magenta: "bg-magenta",
+  orange: "bg-orange",
+  gray: "bg-gray",
+  olive: "bg-olive",
+  darkgreen: "bg-darkgreen",
+  steelblue: "bg-steelblue",
+  lime: "bg-lime",
+  pink: "bg-pink",
+  beige: "bg-beige",
+  teal: "bg-teal",
+};
+
 const Block: React.FC<BlockProps> = ({
   color,
   isSelected = false,
@@ -10,7 +29,7 @@ const Block: React.FC<BlockProps> = ({
   // Tailwind CSS 클래스를 동적으로 적용
   const classes = `
     w-24 h-12 
-    ${color ? `bg-${color}` : "bg-transparent"}
+    ${color ? `${colors[color]}` : "bg-transparent"}
     ${isSelected ? "border-4 border-blue-500" : "border border-black"} 
     inline-block cursor-pointer box-border
   
