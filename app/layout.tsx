73119624ebf9dd5e23a72dcbb03f50ef09ec1e68
thekanon <dlengjs12@naver.com>
2024-05-31 03:09:42 +0900

    배포 테스트2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,11 +13,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <Script
-        src="https://buttr.dev/butter.js"
-        data-site-id="uqvvwxsqdk"
-        async
-      ></Script>
+      <Head>
+        <Script
+          src="https://buttr.dev/butter.js"
+          data-site-id="uqvvwxsqdk"
+          async
+        ></Script>
+      </Head>
 
       <body className={inter.className}>{children}</body>
     </html>
